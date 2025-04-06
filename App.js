1217738adaf6de66351e5e4b73d7ed73a17f1ccc
Charan Tatu <charantatu10@gmail.com>
2025-04-06 09:23:38 +0530

    ex feature removed

--- App.js
+++ App.js
@@ -6,17 +6,13 @@
  * @flow strict-local
  */
 
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {StatusBar} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {Provider} from 'react-redux';
 import {store} from './Componant/assets';
 import { LogBox } from 'react-native'
-import LoginUser from './Componant/LoginUser';
-import DashboardSpringCt from './Componant/DashboardSpringCt';
-import { getTokenFromPref } from './Componant/userpref';
-import EmployeAdd from './Componant/EmployeAdd';
 LogBox.ignoreLogs([
 	'ViewPropTypes will be removed from React Native. Migrate to ViewPropTypes exported from \'deprecated-react-native-prop-types\'.',
 	'NativeBase: The contrast ratio of',
@@ -26,34 +22,12 @@ LogBox.ignoreLogs([
 const Stack = createNativeStackNavigator();
 
 const App = () => {
-  const [token, setToken] = useState(null);
-  useEffect(()=>{
-    getTokenFromPref().then((res)=>{
-      setToken(res)
-    })
-  },[])
 
   return (
     <Provider store={store}>
       <StatusBar backgroundColor="black" />
       <NavigationContainer>
-        <Stack.Navigator initialRouteName={token!=null?'Dashboard':'Login'}>
-        <Stack.Screen
-            name="Dashboard"
-            component={DashboardSpringCt}
-            options={{title: 'Dashboard'}}
-          />
-        <Stack.Screen
-            name="Login"
-            component={LoginUser}
-            options={{title: 'Login'}}
-          />
-
-         <Stack.Screen
-            name="EmployeeAdd"
-            component={EmployeAdd}
-            options={{title: 'EmployeeAdd'}}
-          />
+        <Stack.Navigator>
        
         </Stack.Navigator>
       </NavigationContainer>
